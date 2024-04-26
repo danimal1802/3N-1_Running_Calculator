1b4fb3b25f2630809ba7db76630320e25df89668
Daniel Mitchell <danimal1802@gmail.com>
2024-04-26 08:03:20 +0200

    Update main.cpp
    changed variable types from "long" to "unsigned long"

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // Collatz 3N+1 running calculator
 // Daniel Mitchell
-// 9 April 2024
+// 26 April 2024
 
 #include <LiquidCrystal_I2C.h>
 #include <SPI.h>
@@ -10,14 +10,14 @@
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 // Initial variable settings
-long enn = 10;  // start at 1
-long current_val = enn; 
-long iteration = 0;
-long enn_max = 0;
-long iter_max = 0;
-long enn_at_iter_max;
-long val_max = 0;
-long enn_at_val_max;
+unsigned long enn = 80000;  // startin N value
+unsigned long current_val = enn; 
+unsigned long iteration = 0;
+unsigned long enn_max = 0;
+unsigned long iter_max = 0;
+unsigned long enn_at_iter_max;
+unsigned long val_max = 0;
+unsigned long enn_at_val_max;
 
 void setup() {
   Serial.begin(115200);
@@ -25,10 +25,17 @@ void setup() {
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("3N+1");
+
+  lcd.setCursor(0,1);
+  lcd.print("                    ");
+  lcd.setCursor(0,2);
+  lcd.print("mI=                 ");
+  lcd.setCursor(0,3);
+  lcd.print("mV=                 ");
 }
 
 void loop() {
-
+  //delay(1000);
   lcd.setCursor(6,0);
   lcd.print("N= ");
   lcd.setCursor(8,0);
@@ -56,7 +63,6 @@ void loop() {
     // Update max values
     if (enn >= enn_max) {
       enn_max = enn;
-      //
     }
 
     // Update iteration max and enn at this max
@@ -74,29 +80,32 @@ void loop() {
     lcd.setCursor(0,1);
     lcd.print("                    ");
     lcd.setCursor(0,2);
-    lcd.print("maxI=               ");
+    lcd.print("mI=                 ");
     lcd.setCursor(0,3);
-    lcd.print("maxV=               ");
+    lcd.print("mV=                 ");
     //
     // Otherwis if not converged do this
     // 
     } else {
     iteration = iteration + 1;
     if (current_val % 2 == 0) {
-      lcd.setCursor(16,1);
+      lcd.setCursor(16,0);
       lcd.print("Even");
       current_val = current_val/2;
     } else {
-      lcd.setCursor(16,1);
+      lcd.setCursor(16,0);
       lcd.print("Odd ");
       current_val = (3 * current_val) + 1;  
     }
 
     // Update Max values on LCD screen
-    lcd.setCursor (5,2);  lcd.print(iter_max);
+    lcd.setCursor (3,2);  lcd.print(iter_max);
     lcd.setCursor (14,2); lcd.print(enn_at_iter_max);
-    lcd.setCursor (5,3);  lcd.print(val_max);
+    lcd.setCursor (3,3);  lcd.print(val_max);
     lcd.setCursor (14,3); lcd.print(enn_at_val_max);
+
+  }  
+}
 
   }  
 }
